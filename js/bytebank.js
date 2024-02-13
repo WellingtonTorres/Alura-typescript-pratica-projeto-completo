ba80fc64f52cc296e8acf6a3ad346f69c059962c
WellingtonTorres <wellingtontorresmelo@gmail.com>
2024-02-13 17:01:41 -0300

    concatenação de string js x ts

--- js/bytebank.js
+++ js/bytebank.js
@@ -10,4 +10,37 @@ elementoFormulario.addEventListener("submit", function () {
   if (!elementoFormulario.checkValidity()) {
     alert("Por Favor, preencha todos os campos da transação!");
   }
+
+  const inputTipoTransacao = elementoFormulario.querySelector("#tipoTransacao");
+  const inputValor = elementoFormulario.querySelector("#valor");
+  const inputData = elementoFormulario.querySelector("#data");
+
+  let tipoTransacao = inputTipoTransacao.value;
+  let valor = inputValor.value;
+  let data = inputData.value;
+
+  //valor = Number(valor);
+
+  if (tipoTransacao == "Depósito") {
+    saldo += valor;
+  } else if (
+    tipoTransacao == "Transferência" ||
+    tipoTransacao == "Pagamento de Boleto"
+  ) {
+    saldo -= valor;
+  } else {
+    alert("Tipo de transação inválida!");
+    return;
+  }
+
+  elementoSaldo.textContent = saldo;
+
+  const novaTransacao = {
+    tipoTransacao: tipoTransacao,
+    valor: Number(valor),
+    data: data,
+  };
+
+  console.log(novaTransacao);
+  elementoFormulario.reset();
 });
