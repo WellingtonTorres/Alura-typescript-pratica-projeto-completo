3f9b6c734b6d66558cf3e03fc5f810ec90a3674e
WellingtonTorres <wellingtontorresmelo@gmail.com>
2024-02-16 06:52:53 -0300

    dist/js delete

--- dist/js/saldo-component.js
+++
@@ -1,6 +0,0 @@
-let saldo = 3000;
-alert("teste compilacao");
-const elementoSaldo = document.querySelector(".saldo-valor .valor");
-if (elementoSaldo != null) {
-    elementoSaldo.textContent = saldo.toString();
-}
