1d106a848250ec0c9333f368a0202fa2f6df2944
WellingtonTorres <wellingtontorresmelo@gmail.com>
2024-02-20 07:55:29 -0300

    ajustando demais gets

--- src/types/Conta.ts
+++ src/types/Conta.ts
@@ -1,3 +1,5 @@
+import { GrupoTransacao } from "./GrupoTransacao";
+import { TipoTransacao } from "./TipoTransacao";
 import { Transacao } from "./Transacao";
 
 export class Conta {
@@ -16,6 +18,77 @@ export class Conta {
 
   constructor(nome: string) {
     this.nome = nome;
+  }
+  getGruposTransacoes(): GrupoTransacao[] {
+    const gruposTransacoes: GrupoTransacao[] = [];
+    const listaTrasacoes: Transacao[] = structuredClone(this.transacoes);
+    const transacoesOrdenadas: Transacao[] = listaTrasacoes.sort(
+      (t1, t2) => t2.data.getTime() - t1.data.getTime()
+    );
+    let lebelAtualGrupoTransacao: string = "";
+
+    for (let transacao of transacoesOrdenadas) {
+      let labelGrupoTransacao: string = transacao.data.toLocaleDateString(
+        "pt-br",
+        { month: "long", year: "numeric" }
+      );
+      if (lebelAtualGrupoTransacao != labelGrupoTransacao) {
+        lebelAtualGrupoTransacao = labelGrupoTransacao;
+        gruposTransacoes.push({
+          label: labelGrupoTransacao,
+          transacoes: [],
+        });
+      }
+      gruposTransacoes.at(-1).transacoes.push(transacao);
+    }
+    return gruposTransacoes;
+  }
+
+  getSaldo() {
+    return this.saldo;
+  }
+
+  getDataAcesso(): Date {
+    return new Date();
+  }
+
+  registrarTransacao(novaTransacao: Transacao): void {
+    if (novaTransacao.tipoTransacao == TipoTransacao.DEPOSITO) {
+      this.depositar(novaTransacao.valor);
+    } else if (
+      novaTransacao.tipoTransacao == TipoTransacao.TRANSFERENCIA ||
+      novaTransacao.tipoTransacao == TipoTransacao.PAGAMENTO_BOLETO
+    ) {
+      this.debitar(novaTransacao.valor);
+      novaTransacao.valor *= -1;
+    } else {
+      throw new Error("Tipo de transação inválida!");
+    }
+    this.transacoes.push(novaTransacao);
+    console.log(this.getGruposTransacoes());
+    localStorage.setItem("transacoes", JSON.stringify(this.transacoes));
+  }
+
+  debitar(valor: number): void {
+    if (valor <= 0) {
+      throw new Error("O Valor a ser debitado deve ser maior que zero");
+    }
+
+    if (valor > this.saldo) {
+      throw new Error("Saldo Insuficiente");
+    }
+
+    this.saldo -= valor;
+    localStorage.setItem("saldo", this.saldo.toString());
+  }
+
+  depositar(valor: number): void {
+    if (valor <= 0) {
+      throw new Error("O Valor a ser depositar deve ser maior que zero");
+    }
+
+    this.saldo += valor;
+    localStorage.setItem("saldo", this.saldo.toString());
   }
 }
 
